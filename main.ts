a1a97f7cfd4eb89d0b749f200bef1f913e0ce9cb
rceccan2311 <132548157+rceccan2311@users.noreply.github.com>
2024-06-12 08:55:38 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+    } else if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 200)
+    } else if (receivedNumber == 3) {
+        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    } else if (receivedNumber == 4) {
+        maqueen.motorStop(maqueen.Motors.M1)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    } else if (receivedNumber == 5) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+    } else if (receivedNumber == 6) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+radio.setGroup(1)
+basic.forever(function () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+})
